cc35675e357cd20e8cdfa21cebace9abf41bccfb
rex <anderson.aristotle@gmail.com>
2019-04-15 15:53:25 -0400

    input api production link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://bluewater-private-equity-00001.herokuapp.com',
   development: 'http://localhost:4741'
 }
 

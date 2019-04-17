b1771cfebefd77f2a4a46ed38c01f67171534554
rex <anderson.aristotle@gmail.com>
2019-04-17 13:48:55 -0400

    Built authentication events, ui, and ajax requests

---
+++ assets/scripts/authentication/events.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const getFormFields = require(`../../../lib/get-form-fields`)
+const api = require('./api')
+const ui = require('./ui')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  api.signUp(data)
+    .then(ui.onSignUpSuccess)
+    .catch(ui.onSignUpFailure)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+  const data = getFormFields(this)
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  console.log('change password ran!')
+  const data = getFormFields(this)
+  api.changePassword(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+  console.log('sign out ran')
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onChangePassword,
+  onSignOut
+}
---
+++ assets/scripts/authentication/ui.js
@@ -0,0 +1,70 @@
+'use strict'
+const store = require('../store')
+
+const onSignUpSuccess = function (data) {
+  $('#message').text('Signed up successfully')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+}
+
+const onSignUpFailure = function () {
+  $('#message').text('Error on sign up')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+}
+
+const signInSuccess = function (data) {
+  $('#message').text('Signed in successfully')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  console.log('signInSuccess ran. Data is :', data)
+  store.user = data.user
+}
+
+const signInFailure = function (error) {
+  $('#message').text('Error on sign in')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  console.error('signInFailure ran. Error is :', error)
+}
+
+const changePasswordSuccess = function () {
+  $('#message').text('Changed password successfully')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  console.log('changePasswordSuccess ran and nothing was returned!')
+}
+
+const changePasswordFailure = function (error) {
+  $('#message').text('Error on change password')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  console.error('changePasswordFailure ran. Error is :', error)
+}
+
+const signOutSuccess = function () {
+  $('#message').text('Signed out successfully')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  $('form').trigger('reset')
+  console.log('signOutSuccess ran and nothing was returned!')
+  store.user = null
+}
+
+const signOutFailure = function (error) {
+  $('#message').text('Error on sign out')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  console.error('signOutFailure ran. Error is :', error)
+}
+
+module.exports = {
+  onSignUpSuccess,
+  onSignUpFailure,
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure,
+  changePasswordSuccess,
+  changePasswordFailure
+}

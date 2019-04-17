30ef55b141bfd8b168784bfd9937d840271a0f1b
rex <anderson.aristotle@gmail.com>
2019-04-17 16:27:52 -0400

    created authentication and account create and read events

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,19 @@
 'use strict'
 const authEvents = require('./authentication/events.js')
+const accountEvents = require('./accounts/events.js')
 
 $(() => {
+  // Hide and Show
+  // $('#personal-equity').hide()
+
+  // Authentication
   $('#sign-up-form').on('submit', authEvents.onSignUp)
   $('#sign-in-form').on('submit', authEvents.onSignIn)
   $('#change-password-form').on('submit', authEvents.onChangePassword)
   $('#sign-out-form').on('submit', authEvents.onSignOut)
+
+  // Investment Account
+  $('#create-account-form').on('submit', accountEvents.onCreateAccount)
+  $('#update-account-form').on('submit', accountEvents.onUpdateAccount)
+  $('#read-account-form').on('submit', accountEvents.onGetAccounts)
 })
